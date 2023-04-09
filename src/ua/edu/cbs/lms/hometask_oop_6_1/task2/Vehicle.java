691b84822851223a017118862c7bdc2360335640
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-09 09:47:51 +0300

    Added Vehicle class

---
+++ src/ua/edu/cbs/lms/hometask_oop_6_1/task2/Vehicle.java
@@ -0,0 +1,10 @@
+package ua.edu.cbs.lms.hometask_oop_6_1.task2;
+
+public class Vehicle {
+
+    public void print(){
+        System.out.println("It is print method to Vehicle class.");
+    }
+
+
+}
