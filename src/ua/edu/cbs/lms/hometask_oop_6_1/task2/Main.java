711bc6839911a23d0003a53d3c1f63ec2ba0f240
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-09 09:54:15 +0300

    Added Main class

---
+++ src/ua/edu/cbs/lms/hometask_oop_6_1/task2/Main.java
@@ -0,0 +1,16 @@
+package ua.edu.cbs.lms.hometask_oop_6_1.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Vehicle.Wheel wheel = new Vehicle().new Wheel();
+        Vehicle.Door door = new Vehicle().new Door();
+
+        vehicle.print();
+        wheel.print();
+        door.print();
+
+        //Vehicle.Wheel wheel1 = new Vehicle().new Wheel();
+
+    }
+}
