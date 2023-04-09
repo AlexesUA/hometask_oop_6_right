9310b1fe1f4a4a3dc7c5a18567e953569d6b34c2
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-09 10:36:18 +0300

    Created Animals enum

---
+++ src/ua/edu/cbs/lms/hometask_oop_6_2/task_2/Animals.java
@@ -0,0 +1,18 @@
+package ua.edu.cbs.lms.hometask_oop_6_2.task_2;
+
+public enum Animals {
+    BEAR(12),TURTLES(129),LEOPARD(5);
+
+
+    private int age;
+
+    Animals(int age){
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("This animal is %1$s, his %2$s years old.",this,age);
+    }
+}
