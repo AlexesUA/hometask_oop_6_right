7e02c51f4398f15665775b374dabb0bf85f9e5ac
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-09 10:18:48 +0300

    Created Distance class

---
+++ src/ua/edu/cbs/lms/hometask_oop_6_1/task3/Distance.java
@@ -0,0 +1,28 @@
+package ua.edu.cbs.lms.hometask_oop_6_1.task3;
+
+public class Distance {
+    double distance;
+
+    public Distance(double distance){
+        this.distance = distance;
+    }
+
+    public void print(){
+        System.out.println("Distance = " + distance);
+    }
+
+    public static class Converter{
+        public static double convertFromMetersToKilometers(double distance){
+            return distance/1000;
+        }
+
+        public static double convertFromKilometersToMiles(double distance){
+            return distance * 0.621371;
+        }
+
+        public static double convertFromKilometersToFeet(double distance){
+            return distance * 3280.839895;
+        }
+    }
+
+}
